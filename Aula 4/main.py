bfdd74f1e2a9d06455c5809cb3ca7c2fe6b32507
Thais Novaes <thaisnovaes.ar@gmail.com>
2025-08-28 16:59:42 -0300

    .

---
+++ Aula 4/main.py
@@ -0,0 +1,17 @@
+nota = int(input("Digite a sua nota: "))
+nota2 = int(input("Digite a sua nota: "))
+
+media = (nota + nota2) / 2
+
+if media <= 70 :
+    print("Aprovado")
+    
+elif media >= 50 :
+    print("Recuperação")
+    recuperacao = int(input("Digite a nota de recuperação: "))
+    media2 = (media + recuperacao) / 2
+    
+    if 
+    
+else:
+    print("Reprovado")

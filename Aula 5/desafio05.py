a8ce1cc071080bb372ae590cd9656a88b99276a9
Thais Novaes <thaisnovaes.ar@gmail.com>
2025-09-02 16:55:37 -0300

    .

--- Aula 5/desafio05.py
+++ Aula 5/desafio05.py
@@ -6,3 +6,23 @@ usuario = input("Hora de jogar! Digite Pedra, Papel ou Tesoura: ")
 
 print(maquina)
 
+if maquina == "Pedra" and usuario == "Papel":
+    print("Parabéns! Você venceu o jogo")
+    
+elif maquina == "Pedra" and usuario == "Tesoura":
+    print("Você perdeu! tente novamente")
+    
+elif maquina == "Papel" and usuario == "Tesoura": 
+    print("Parabéns! Você venceu o jogo") 
+    
+elif maquina == "Papel" and usuario == "Pedra":
+    print("Você perdeu! tente novamente")
+    
+elif maquina == "Tesoura" and usuario == "Papel":
+    print("Você perdeu! tente novamente")
+    
+elif maquina == "Tesoura" and usuario == "Pedra":
+    print("Parabéns! Você venceu o jogo")
+    
+else: 
+    print("Você empatou o jogo com a máquina!")
